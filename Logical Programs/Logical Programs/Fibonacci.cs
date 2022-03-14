a89975d5864f6a58a7e7504f78670ff49c1590b9
unknown <kaithwas.697@gmail.com>
2022-03-14 21:12:25 +0530

    Program to find Fibonacci Series

---
+++ Logical Programs/Logical Programs/Fibonacci.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Logical_Programs
+{
+    internal class Fibonacci
+    {
+        public void FibonacciSeries()
+        {
+            Console.WriteLine("Enter a number to get fibonacci series: ");
+            int number = int.Parse(Console.ReadLine());
+            int firstNumber = 0;
+            int secondNumber = 1;
+            int result;
+            if (number <= 1)
+            {
+                Console.WriteLine(firstNumber + "  " + secondNumber + " ");
+            }
+            else
+            {
+                Console.Write(firstNumber + " " + secondNumber + " ");
+                for (int i = 2; i < number; i++)
+                {
+                    result = firstNumber + secondNumber;
+                    Console.Write(result + " ");
+                    firstNumber = secondNumber;
+                    secondNumber = result;
+                }
+            }
+        }
+    }
+}
---
+++ Logical Programs/Logical Programs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Logical_Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose 1: Fibonacci series");
+            int option = int.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Fibonacci fibonacci = new Fibonacci();
+                    fibonacci.FibonacciSeries();
+                    break;
+                default:
+                    Console.WriteLine("Enter valid option");
+                    break;
+            }
+        }
+    }
+}
