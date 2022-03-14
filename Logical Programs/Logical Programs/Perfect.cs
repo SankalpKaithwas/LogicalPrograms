76e85879d898170f704b702cd9013ce53cffb44f
unknown <kaithwas.697@gmail.com>
2022-03-14 21:19:01 +0530

    Added feature to check whether Number is Perfect or not

---
+++ Logical Programs/Logical Programs/Perfect.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Logical_Programs
+{
+    internal class Perfect
+    {
+        public void PerfectNumber()
+        {
+            Console.WriteLine("Enter a number");
+            int number = int.Parse(Console.ReadLine());
+            int temp = number;
+            int div = 0;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    div += i;
+                }
+            }
+            if (temp == div)
+            {
+                Console.WriteLine($"{temp} is a perfect number");
+            }
+            else
+            {
+                Console.WriteLine($"{temp} is not a perfect number");
+            }
+        }
+    }
+}
--- Logical Programs/Logical Programs/Program.cs
+++ Logical Programs/Logical Programs/Program.cs
@@ -7,12 +7,17 @@ namespace Logical_Programs
         static void Main(string[] args)
         {
             Console.WriteLine("Choose 1: Fibonacci series");
+            Console.WriteLine("Choose 2: Perfect Number");
             int option = int.Parse(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     Fibonacci fibonacci = new Fibonacci();
                     fibonacci.FibonacciSeries();
+                    break;
+                case 2:
+                    Perfect perfect = new Perfect();
+                    perfect.PerfectNumber();
                     break;
                 default:
                     Console.WriteLine("Enter valid option");
